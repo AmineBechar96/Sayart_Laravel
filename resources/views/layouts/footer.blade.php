d36daa4422f28460ef86a79c1d534e7c7245c4d8
AmineBechar96 <47677152+AmineBechar96@users.noreply.github.com>
2021-12-29 18:14:58 +0100

    Add files via upload

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,83 @@
+<!-- ======= Footer ======= -->
+<footer id="footer">
+    <div class="footer-top">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-lg-3 col-md-6">
+                    <div class="footer-info">
+                        <h3>Dzautotrader</h3>
+                        <p>
+                            A108 Adam Street <br>
+                            NY 535022, USA<br><br>
+                            <strong>Phone:</strong> +1 5589 55488 55<br>
+                            <strong>Email:</strong> info@example.com<br>
+                        </p>
+                        <div class="social-links mt-3">
+                            <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+                            <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+                            <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+
+                            <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-2 col-md-6 footer-links">
+                    <h4>Liens utiles
+                    </h4>
+                    <ul>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Accueil</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Estimer Valeur</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Verifier l'Etat</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Recherche Avancé</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Statistique</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Suivez le Marché</a></li>
+
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-lg-3 col-md-6 footer-links">
+                    <h4>Notre Services</h4>
+                    <ul>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Location de voitures</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Mécanique Générale</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Pièces Détachés</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Depannage & Transport</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Points de Ventes</a>
+                        </li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Marchés Hebdomadaires</a>
+                        </li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">News</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-lg-4 col-md-6 footer-newsletter">
+                    <h4>Our Newsletter</h4>
+                    <p>Inscrivez-vous pour recevoir des nouvelles sur l'annonce de nouveaux produits, les cadeaux, les
+                        promotions spéciales, les ventes et plus encore
+                    </p>
+                    <form action="" method="post">
+                        <input type="email" name="email"><input type="submit" value="Subscribe">
+                    </form>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="copyright">
+            &copy; Copyright <strong><span>sayarti</span></strong>. All Rights Reserved
+        </div>
+        <div class="credits">
+            <!-- All the links in the footer should remain intact. -->
+            <!-- You can delete the links only if you purchased the pro version. -->
+            <!-- Licensing information: https://bootstrapmade.com/license/ -->
+            <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/medicio-free-bootstrap-theme/ -->
+            Designed by <a href="#">Innogate</a>
+        </div>
+    </div>
+</footer><!-- End Footer -->
